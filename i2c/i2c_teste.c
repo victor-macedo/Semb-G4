d63a5d6ec627c423c1c6c30153e8a35be10dfd35
vascoluz <114610441+vascoluz@users.noreply.github.com>
2022-12-28 14:35:40 +0000

    Update i2c_teste.c

--- i2c/i2c_teste.c
+++ i2c/i2c_teste.c
@@ -1,37 +1,19 @@
-#include<stdint.h>
-#include<stdbool.h>
-#include"inc/tm4c123gh6pm.h"
-#include "inc/hw_memmap.h"
-#include "driverlib/gpio.h"
-#include "inc/hw_types.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/i2c.h"
+#include <stdbool.h>
+#include <stdint.h>
 #include "inc/hw_i2c.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/i2c.h"
 #include "driverlib/pin_map.h"
-
-/**
- * main.c
- */
-
-
-void I2CSend(uint8_t slave_addr, uint8_t message,uint8_t message2){
-
-    I2CMasterSlaveAddrSet(I2C1_BASE, slave_addr, false);
-
-    I2CMasterDataPut(I2C1_BASE, message);
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "utils/softi2c.h"
+//#define NUM_I2C_DATA 3
+#define SLAVE_ADDRESS 0x3C
 
 
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
-    while(I2CMasterBusy(I2C1_BASE));
-
-    I2CMasterDataPut(I2C1_BASE, message2);
-
-
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
-
-
-    while(I2CMasterBusy(I2C1_BASE));
-}
 
 
 
@@ -45,11 +27,22 @@ void I2CSend(uint8_t slave_addr, uint8_t message,uint8_t message2){
 
 int main(void)
 {
+
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+
     SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
-    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
+
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+
+
     GPIOPinConfigure(GPIO_PA6_I2C1SCL);
     GPIOPinConfigure(GPIO_PA7_I2C1SDA);
+
+
+
+
 
     GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
     GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
@@ -59,9 +52,11 @@ int main(void)
 
 
     while(1){
-        I2CSend(0x33, 0xaa,0x38);
-
-
+        I2CMasterSlaveAddrSet(I2C1_BASE, 0x23, false);
+        I2CMasterDataPut(I2C1_BASE, 0xAA);
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+        while(I2CMasterBusy(I2C1_BASE));
     }
 	return 0;
 }
+
