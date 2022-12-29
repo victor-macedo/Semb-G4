02af76d6973a58ae590e87efd7d5ed104ab59f4e
Victor S Macedo <guivi2000.vm@gmail.com>
2022-12-29 15:34:05 +0000

    I2C funcionando
    Falta porem configurar o sensor

---
+++ i2c/I2c.c
@@ -0,0 +1,84 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_i2c.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/i2c.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "utils/softi2c.h"
+//#define NUM_I2C_DATA 3
+#define SLAVE_ADDRESS 0x3C
+
+
+
+void I2CSend(uint32_t slave_addr, uint8_t reg){
+    I2CMasterSlaveAddrSet(I2C1_BASE, slave_addr, false);
+    I2CMasterDataPut(I2C1_BASE, reg);
+    SysCtlDelay(1000);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    SysCtlDelay(1000);
+    while(I2CMasterBusy(I2C1_BASE));
+}
+
+
+uint32_t I2CReceive(uint32_t slave_addr)
+{
+    //specify that we are writing (a register address) to the
+    //slave device
+    I2CMasterSlaveAddrSet(I2C1_BASE, slave_addr, false);
+
+    //specify register to be read
+    I2CMasterDataPut(I2C1_BASE, 0XFF);
+
+    //send control byte and register address byte to slave device
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+
+    //wait for MCU to finish transaction
+    while(I2CMasterBusy(I2C1_BASE));
+
+    //specify that we are going to read from slave device
+    I2CMasterSlaveAddrSet(I2C1_BASE, slave_addr, true);
+
+    //send control byte and read from the register we
+    //specified
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
+
+    //wait for MCU to finish transaction
+    while(I2CMasterBusy(I2C1_BASE));
+
+    //return data pulled from the specified register
+    return I2CMasterDataGet(I2C1_BASE);
+}
+
+
+
+
+
+
+int main(void)
+{
+    uint32_t temp;
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
+    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    GPIOPinConfigure(GPIO_PA6_I2C1SCL);
+    GPIOPinConfigure(GPIO_PA7_I2C1SDA);
+
+    GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
+    GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
+
+    I2CMasterInitExpClk(I2C1_BASE, SysCtlClockGet(), false);
+
+
+    while(1){
+        I2CSend(SLAVE_ADDRESS,0xAA);
+        temp = I2CReceive(SLAVE_ADDRESS);
+    }
+    return 0;
+}
