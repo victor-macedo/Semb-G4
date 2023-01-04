744c1d37e8e748b1692aa69b732ecd0213c1f60c
vascoluz <114610441+vascoluz@users.noreply.github.com>
2023-01-04 00:57:46 +0000

    Update I2c.c
    com os registos, precisa de uns ajustes

--- i2c/I2c.c
+++ i2c/I2c.c
@@ -11,8 +11,15 @@
 #include "utils/uartstdio.h"
 #include "utils/softi2c.h"
 //#define NUM_I2C_DATA 3
-#define SLAVE_ADDRESS 0x48
+#define SLAVE_ADDRESS_WRITE 0x90
+#define SLAVE_ADDRESS_READ 0x91
+#define CONFIG_TMP 0x01
+#define CONFIG_TMP_BITS 0x00
+#define TEMP_REG 0x00
 
+
+
+uint32_t temp;
 
 
 void I2CSend(uint32_t slave_addr, uint8_t reg){
@@ -21,6 +28,37 @@ void I2CSend(uint32_t slave_addr, uint8_t reg){
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
     while(I2CMasterBusy(I2C1_BASE));
 }
+
+
+void I2CSENDCONFIG(){
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDRESS_WRITE, false);
+    I2CMasterDataPut(I2C1_BASE, CONFIG_TMP);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(I2CMasterBusy(I2C1_BASE));
+    I2CMasterDataPut(I2C1_BASE, CONFIG_TMP_BITS);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH );
+    while(I2CMasterBusy(I2C1_BASE));
+}
+
+
+void I2C_get_temp(){
+    uint32_t temp1, temp2;
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDRESS_WRITE, false);
+    I2CMasterDataPut(I2C1_BASE, TEMP_REG);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+    while(I2CMasterBusy(I2C1_BASE)); // delay de 40 ms
+
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDRESS_READ, false);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_FIFO_BURST_RECEIVE_START);
+    while(I2CMasterBusy(I2C1_BASE));
+    temp1 = I2CMasterDataGet(I2C1_BASE);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_FIFO_BURST_RECEIVE_FINISH);
+    while(I2CMasterBusy(I2C1_BASE));
+    temp2 = I2CMasterDataGet(I2C1_BASE);
+    temp = (temp1<<8|temp2);//nao sei se esta certp
+}
+
+
 
 
 uint32_t I2CReceive(uint32_t slave_addr)
@@ -57,8 +95,9 @@ uint32_t I2CReceive(uint32_t slave_addr)
 
 
 
-int I2CInit(void)
+int main(void)
 {
+    uint32_t temp;
     SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
     SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
 
@@ -72,5 +111,12 @@ int I2CInit(void)
 
     I2CMasterInitExpClk(I2C1_BASE, SysCtlClockGet(), false);
 
+
+    void I2CSENDCONFIG(); //configuracao do i2c
+
+
+    while(1){
+        I2C_get_temp();
+    }
     return 0;
 }
